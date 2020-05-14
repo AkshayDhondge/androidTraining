6ac516875be71d66dd5a8e93b9262e4c9ec11761
akshay.dhondge <akshay.dhondge@cuelogic>
2020-05-14 19:10:32 +0530

    Basics design

--- app/src/main/java/com/example/firstandroidapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/firstandroidapp/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.firstandroidapp
+package com.example.firstandroidapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.firstandroidapp.R
 
 class MainActivity : AppCompatActivity() {
 
